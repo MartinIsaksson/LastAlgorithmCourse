4ec50060176714604d3d615c0ff2b56530607999
Martin Isaksson <martin_isaksson@hotmail.com>
2022-10-08 21:33:29 +0200

    more algos

--- src/day1/Queue.ts
+++ src/day1/Queue.ts
@@ -1,18 +1,48 @@
+type Node<T> = {
+    value: T;
+    next?: Node<T>;
+};
 export default class Queue<T> {
     public length: number;
-
-    
+    private head?: Node<T>;
+    private tail?: Node<T>;
 
     constructor() {
+        this.head = this.tail = undefined;
+        this.length = 0;
     }
 
     enqueue(item: T): void {
-
-}
+        this.length++;
+        const node = { value: item } as Node<T>;
+        //in other languages we check the length parameter instead
+        if (!this.tail) {
+            this.tail = this.head = node;
+            return;
+        }
+        this.tail.next = node;
+        this.tail = node;
+    }
     deque(): T | undefined {
+        if (!this.head) {
+            return undefined;
+        }
 
-}
+        this.length--;
+        const head = this.head;
+        this.head = this.head.next;
+
+        //Book keeping, free memory (not needed in JS)
+        head.next = undefined;
+
+        //tail?
+        if (this.length === 0) {
+            this.tail = undefined;
+        }
+
+        return head.value;
+    }
     peek(): T | undefined {
-
-}
+        return this.head?.value;
+    }
 }
